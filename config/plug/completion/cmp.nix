80ca8d78497ee32dd4a5f998b12d51abb3379063
AlxTray <alex.ashby02@icloud.com>
2024-08-18 22:46:24 +0100

    feat: updated cmp with extra sources

--- config/plug/completion/cmp.nix
+++ config/plug/completion/cmp.nix
@@ -1,55 +1,46 @@
 {
   plugins = {
-    cmp-emoji = {
-      enable = true;
-    };
     cmp = {
       enable = true;
       settings = {
         autoEnableSources = true;
-        experimental = {
-          ghost_text = true;
-        };
+        experimental.ghost_text = true;
         performance = {
           debounce = 60;
           fetchingTimeout = 200;
           maxViewEntries = 30;
         };
-        snippet = {
-          expand = "luasnip";
-        };
+        snippet.expand = "luasnip";
         formatting = {
           fields = [
-            "kind"
             "abbr"
+            "kind"
             "menu"
           ];
         };
         sources = [
           { name = "nvim_lsp"; }
+          { name = "cmp_git"; }
+          { name = "cmdline"; }
           { name = "emoji"; }
           {
-            name = "buffer"; # text within current buffer
+            name = "buffer";
             option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
             keywordLength = 3;
           }
           {
-            name = "path"; # file system paths
+            name = "path";
             keywordLength = 3;
           }
           {
-            name = "luasnip"; # snippets
+            name = "luasnip";
             keywordLength = 3;
           }
         ];
 
         window = {
-          completion = {
-            border = "rounded";
-          };
-          documentation = {
-            border = "rounded";
-          };
+          completion.border = "rounded";
+          documentation.border = "rounded";
         };
 
         mapping = {
@@ -65,82 +56,68 @@
         };
       };
     };
-    cmp-nvim-lsp = {
-      enable = true;
-    }; # lsp
-    cmp-buffer = {
-      enable = true;
-    };
-    cmp-path = {
-      enable = true;
-    }; # file system paths
-    cmp_luasnip = {
-      enable = true;
-    }; # snippets
-    cmp-cmdline = {
-      enable = false;
-    }; # autocomplete for cmdline
+    cmp-nvim-lsp.enable = true;
+    cmp-buffer.enable = true;
+    cmp-path.enable = true;
+    cmp_luasnip.enable = true;
+    cmp-cmdline.enable = true;
+    cmp-emoji.enable = true;
+    cmp-git.enable = true;
   };
   extraConfigLua = ''
-          luasnip = require("luasnip")
-          kind_icons = {
-            Text = "󰊄",
-            Method = "",
-            Function = "󰡱",
-            Constructor = "",
-            Field = "",
-            Variable = "󱀍",
-            Class = "",
-            Interface = "",
-            Module = "󰕳",
-            Property = "",
-            Unit = "",
-            Value = "",
-            Enum = "",
-            Keyword = "",
-            Snippet = "",
-            Color = "",
-            File = "",
-            Reference = "",
-            Folder = "",
-            EnumMember = "",
-            Constant = "",
-            Struct = "",
-            Event = "",
-            Operator = "",
-            TypeParameter = "",
-          } 
+        luasnip = require("luasnip")
+        kind_icons = {
+          Text = "󰊄",
+          Method = "",
+          Function = "󰡱",
+          Constructor = "",
+          Field = "",
+          Variable = "󱀍",
+          Class = "",
+          Interface = "",
+          Module = "󰕳",
+          Property = "",
+          Unit = "",
+          Value = "",
+          Enum = "",
+          Keyword = "",
+          Snippet = "",
+          Color = "",
+          File = "",
+          Reference = "",
+          Folder = "",
+          EnumMember = "",
+          Constant = "",
+          Struct = "",
+          Event = "",
+          Operator = "",
+          TypeParameter = "",
+        } 
 
-           local cmp = require'cmp'
+         local cmp = require'cmp'
 
-       -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-       cmp.setup.cmdline({'/', "?" }, {
-         sources = {
-           { name = 'buffer' }
-         }
+     -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+     cmp.setup.cmdline({'/', "?" }, {
+       sources = {
+         { name = 'buffer' }
+       }
+     })
+
+    -- Set configuration for specific filetype.
+     cmp.setup.filetype('gitcommit', {
+       sources = cmp.config.sources({
+         { name = 'cmp_git' },
+       }, {
+         { name = 'buffer' },
        })
+     })
 
-      -- Set configuration for specific filetype.
-       cmp.setup.filetype('gitcommit', {
-         sources = cmp.config.sources({
-           { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-         }, {
-           { name = 'buffer' },
-         })
-       })
-
-       -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-       cmp.setup.cmdline(':', {
-         sources = cmp.config.sources({
-           { name = 'path' }
-         }, {
-           { name = 'cmdline' }
-         }),
-    --      formatting = {
-    --       format = function(_, vim_item)
-    --         vim_item.kind = cmdIcons[vim_item.kind] or "FOO"
-    --       return vim_item
-    --      end
-    -- }
-       })  '';
+     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+     cmp.setup.cmdline(':', {
+       sources = cmp.config.sources({
+         { name = 'path' }
+       }, {
+         { name = 'cmdline' }
+       }),
+     })  '';
 }
