5d6cb0753e38dbf2ac6ac5842f1328fd00b8c65f
AlxTray <alex.ashby02@icloud.com>
2024-08-31 11:34:47 +0100

    sets: added new and fixed old

--- config/sets.nix
+++ config/sets.nix
@@ -1,92 +1,96 @@
 {
-  config = {
-    opts = {
-      defaultEditor = true;
+  viAlias = true;
+  vimAlias = true;
 
-      # Enable relative line numbers
-      number = true;
-      relativenumber = true;
+  clipboard = {
+    register = "unnamedplus";
+    providers.wl-copy.enable = true;
+  };
 
-      # Set tabs to 2 spaces
-      tabstop = 2;
-      softtabstop = 2;
-      showtabline = 2;
-      expandtab = true;
+  opts = {
+    # Enable relative line numbers
+    number = true;
+    relativenumber = true;
 
-      # Enable auto indenting and set it to spaces
-      smartindent = true;
-      shiftwidth = 2;
+    # Set tabs to 2 spaces
+    tabstop = 2;
+    softtabstop = 2;
+    showtabline = 2;
+    expandtab = true;
 
-      # Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
-      breakindent = true;
+    # Enable auto indenting and set it to spaces
+    smartindent = true;
+    shiftwidth = 2;
 
-      # Enable incremental searching
-      hlsearch = true;
-      incsearch = true;
+    # Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
+    breakindent = true;
 
-      # Enable text wrap
-      wrap = true;
+    # Enable incremental searching
+    hlsearch = true;
+    incsearch = true;
 
-      # Better splitting
-      splitbelow = true;
-      splitright = true;
+    # Enable text wrap
+    wrap = true;
 
-      # Enable mouse mode
-      mouse = "a"; # Mouse
+    # Better splitting
+    splitbelow = true;
+    splitright = true;
 
-      # Enable ignorecase + smartcase for better searching
-      ignorecase = true;
-      smartcase = true; # Don't ignore case with capitals
-      grepprg = "rg --vimgrep";
-      grepformat = "%f:%l:%c:%m";
+    # Enable mouse mode
+    mouse = "a"; # Mouse
 
-      # Decrease updatetime
-      updatetime = 50; # faster completion (4000ms default)
+    # Enable ignorecase + smartcase for better searching
+    ignorecase = true;
+    smartcase = true; # Don't ignore case with capitals
+    grepprg = "rg --vimgrep";
+    grepformat = "%f:%l:%c:%m";
 
-      # Set completeopt to have a better completion experience
-      completeopt = [
-        "menuone"
-        "noselect"
-        "noinsert"
-      ]; # mostly just for cmp
+    # Decrease updatetime
+    updatetime = 50; # faster completion (4000ms default)
 
-      # Enable persistent undo history
-      swapfile = false;
-      backup = false;
-      undofile = true;
+    # Set completeopt to have a better completion experience
+    completeopt = [
+      "menuone"
+      "noselect"
+      "noinsert"
+    ]; # mostly just for cmp
 
-      # Enable 24-bit colors
-      termguicolors = true;
+    # Enable persistent undo history
+    swapfile = false;
+    backup = false;
+    undofile = true;
 
-      # Enable the sign column to prevent the screen from jumping
-      signcolumn = "yes";
+    # Enable 24-bit colors
+    termguicolors = true;
 
-      # Enable cursor line highlight
-      cursorline = true; # Highlight the line where the cursor is located
+    # Enable the sign column to prevent the screen from jumping
+    signcolumn = "yes";
 
-      # Set fold settings
-      # These options were reccommended by nvim-ufo
-      # See: https://github.com/kevinhwang91/nvim-ufo#minimal-configuration
-      foldcolumn = "0";
-      foldlevel = 99;
-      foldlevelstart = 99;
-      foldenable = true;
+    # Enable cursor line highlight
+    cursorline = true; # Highlight the line where the cursor is located
 
-      # Always keep 8 lines above/below cursor unless at start/end of file
-      scrolloff = 8;
+    # Set fold settings
+    # These options were reccommended by nvim-ufo
+    # See: https://github.com/kevinhwang91/nvim-ufo#minimal-configuration
+    foldcolumn = "0";
+    foldlevel = 99;
+    foldlevelstart = 99;
+    foldenable = true;
 
-      # Reduce which-key timeout to 10ms
-      timeoutlen = 10;
+    # Always keep 8 lines above/below cursor unless at start/end of file
+    scrolloff = 8;
 
-      # Set encoding type
-      encoding = "utf-8";
-      fileencoding = "utf-8";
+    # Reduce which-key timeout to 10ms
+    timeoutlen = 10;
 
-      # More space in the neovim command line for displaying messages
-      cmdheight = 0;
+    # Set encoding type
+    encoding = "utf-8";
+    fileencoding = "utf-8";
 
-      # We don't need to see things like INSERT anymore
-      showmode = false;
-    };
+    # More space in the neovim command line for displaying messages
+    cmdheight = 0;
+
+    # We don't need to see things like INSERT anymore
+    showmode = false;
   };
 }
